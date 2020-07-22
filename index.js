9e958204ebb8d5f04ceb263d4a95c8f570ceec63
Akshay Niranjan <anstmfriend@gmail.com>
2020-07-22 06:37:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(line){
   return `Currently serving ${line.shift()}.`
 }
 function currentLine(line){
-  if(line.length){
+  if(line.length==0){
     return "The line is currently empty."
   }
   let i=0
